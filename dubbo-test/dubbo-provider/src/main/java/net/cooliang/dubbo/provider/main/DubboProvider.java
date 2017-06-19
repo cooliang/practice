ef9a3f33f07e762ded150b0df9db7a787a6cded4
梁擎 <liangqing1990@126.com>
2017-06-19 13:19:45 +0800

    修改dubbo服务端启动程序

--- dubbo-test/dubbo-provider/src/main/java/net/cooliang/dubbo/provider/main/DubboProvider.java
+++ dubbo-test/dubbo-provider/src/main/java/net/cooliang/dubbo/provider/main/DubboProvider.java
@@ -1,16 +1,11 @@
 package net.cooliang.dubbo.provider.main;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.alibaba.dubbo.container.Main;
 
 public class DubboProvider {
 
 	public static void main(String[] args) throws InterruptedException {
-		try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-				new String[] { "spring/spring-context.xml", "spring/spring-rpc.xml" })) {
-			while (true) {
-				Thread.sleep(300 * 1000);
-			}
-		}
+		Main.main(args);
 	}
 
 }
